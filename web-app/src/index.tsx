b1fd3356ce07eef7617581973690c7b116cd2fad
Katelyn Perretta @ Monulox Capital <katelyn.p@monulox.capital>
2025-10-17 05:13:16 +0000

    Commit mistakes made previously that were corrected after ChatGPT analysis

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -7,13 +7,14 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
+// ✅ Enable PWA support
+// To make your app work offline and load faster, change unregister() to register() below.
+// Learn more: https://cra.link/PWA
 serviceWorkerRegistration.register();
